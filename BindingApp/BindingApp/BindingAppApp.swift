fe86e720bbaccf5eb9e35be583aaeba97898eee9
UlianaPereverzeva <115875780+UlianaPereverzeva@users.noreply.github.com>
2023-02-15 19:53:32 +0300

    project created

---
+++ BindingApp/BindingApp/BindingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingAppApp.swift
+//  BindingApp
+//
+//  Created by ульяна on 15.02.23.
+//
+
+import SwiftUI
+
+@main
+struct BindingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
